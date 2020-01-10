2e6ae8973312fa42fea894b6b8c066bd84428569
Sammy <ksamy90@aol.com>
2020-01-10 14:36:32 +0300

    remove zip files

--- assets/js/incubation_script.js
+++ assets/js/incubation_script.js
@@ -1,20 +1,19 @@
-$(document).on('ready', function () {
+$(document).on("ready", function() {
+  $(".incubation-menu").on("click", function(e) {
+    const nav = $(".incubation-nav-sm");
+    nav.removeClass("d-none");
+    nav.addClass("d-flex ham");
+  });
 
-    $('.incubation-menu').on('click', function (e) {
-        const nav = $('.incubation-nav-sm');
-        nav.removeClass("d-none");
-        nav.addClass("d-flex");
-    });
+  $(".close").on("click", function(e) {
+    const nav = $(".incubation-nav-sm");
+    nav.removeClass("d-flex ham");
+    nav.addClass("d-none");
+  });
 
-    $('.close').on('click', function (e) {
-        const nav = $('.incubation-nav-sm');
-        nav.removeClass("d-flex");
-        nav.addClass("d-none");
-    });
-
-    $('.close2').on('click', function (e) {
-        const nav = $('.incubation-nav-sm');
-        nav.removeClass("d-flex");
-        nav.addClass("d-none");
-    });
+  $(".close2").on("click", function(e) {
+    const nav = $(".incubation-nav-sm");
+    nav.removeClass("d-flex");
+    nav.addClass("d-none");
+  });
 });
