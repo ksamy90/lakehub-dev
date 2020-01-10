4a1471c724d5be2fb90ebf278d3fc61eb7349020
ksamy90 <ksamy90@aol.com>
2020-01-09 22:01:14 -0800

    add close effect on hamburger icon

--- assets/js/incubation_script.js
+++ assets/js/incubation_script.js
@@ -11,4 +11,10 @@ $(document).on('ready', function () {
         nav.removeClass("d-flex");
         nav.addClass("d-none");
     });
+
+    $('.close2').on('click', function (e) {
+        const nav = $('.incubation-nav-sm');
+        nav.removeClass("d-flex");
+        nav.addClass("d-none");
+    });
 });
